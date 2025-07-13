30d99dac07f5914fe15171ea68e86f51af24de85
dennislee22 <35444414+dennislee22@users.noreply.github.com>
2025-07-13 17:19:56 +0800

    Update create-synthetic-cdr.py

--- create-synthetic-cdr.py
+++ create-synthetic-cdr.py
@@ -53,7 +53,7 @@ def generate_batch_data(msisdn_batch: List[int], fraud_msisdns: set):
     return pd.DataFrame(batch_records)
 
 if __name__ == '__main__':
-    NUM_USERS = 265000
+    NUM_USERS = 140000
     FRAUD_PERCENTAGE = 0.05
     BATCH_SIZE = 500   # Process 500 users at a time to keep memory low
     OUTPUT_FILENAME = '3Gcdr_data.csv'
